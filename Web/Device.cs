405311361132e49d19ddfc757ec045e76cd5e43c
renyi <421643133@qq.com>
2018-08-23 09:14:53 +0800

    增加web移动端设备是被

---
+++ Web/Device.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace RX.Web
+{
+    public class Device
+    {
+        /// <summary>
+        /// 判断是否移动设备。
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsMobileDevice()
+        {
+            if (HttpContext.Current.IsNull())
+                return false;
+
+            string[] mobileAgents = { "iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi", "opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod", "nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma", "docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos", "techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem", "wellcom", "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos", "pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320", "240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac", "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs", "kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi", "mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port", "prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem", "smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v", "voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-", "googlebot-mobile" };
+
+            bool isMoblie = false;
+
+            string userAgent = HttpContext.Current.Request.UserAgent.ToString().ToLower();
+
+            //排除 Windows 桌面系统或苹果桌面系统 
+            if (!string.IsNullOrEmpty(userAgent) && !userAgent.Contains("macintosh") && (!userAgent.Contains("windows nt") || (userAgent.Contains("windows nt") && userAgent.Contains("compatible; msie 9.0;"))))
+            {
+                for (int i = 0; i < mobileAgents.Length; i++)
+                {
+                    if (userAgent.ToLower().IndexOf(mobileAgents[i]) >= 0)
+                    {
+                        isMoblie = true;
+                        break;
+                    }
+                }
+            }
+
+            return isMoblie;
+        }
+    }
+}
